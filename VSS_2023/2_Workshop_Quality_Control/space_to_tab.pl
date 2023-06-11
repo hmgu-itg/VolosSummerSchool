dcdddd415d56fe030e017e54bfab389b2a6377a8
Arruda <ana.arruda@helmholtz-munich.de>
2023-06-11 18:04:23 +0200

    updated workshop1a, 1b, 2 and lecture 2

--- VSS_2023/2_Workshop_Quality_Control/space_to_tab.pl
+++
@@ -1,9 +0,0 @@
-open INPUT, "$ARGV[0]" or die "Unable to open Input file\n";
-open OUTPUT, ">tab-$ARGV[0]" or die "Unable to open output file\n";
-while (<INPUT>)
- {
- chomp;
- s/^\s+//;
- s/\s+/\t/g;
- print OUTPUT "$_\n";
- }
